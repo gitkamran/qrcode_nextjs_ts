e456646dc19a8c2c1939e5f926148d3708331325
gitkamran <mkamran.ir@gmail.com>
2025-09-29 10:17:10 +0330

    add new font, header and top section responsive for all platforme

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -5,13 +5,17 @@ import { PiGithubLogo } from "react-icons/pi";
 const Header = () => {
   return (
     <header className="w-full border-b border-b-neutral-300">
-      <div className="container mx-auto py-4">
+      <div className="container mx-auto p-2 sm:px-0">
         <div className="flex items-center justify-between">
           <div className="flex flex-col gap-0">
-            <span className="font-bold text-xl">ساخت QR Code</span>
-            <span className="text-lg text-neutral-500">سریع و رایگان</span>
+            <span className="md:text-lg 2xl:text-xl font-bold">ساخت QR Code</span>
+            <span className="text-sm md:text-base 2xl:text-lg text-neutral-500">سریع و رایگان</span>
           </div>
-          <Link href="https://github.com/gitkamran" target="_blank" className="flex items-center gap-1 text-neutral-800">
+          <Link
+            href="https://github.com/gitkamran"
+            target="_blank"
+            className="flex items-center justify-center gap-1 text-neutral-800 bg-neutral-200"
+          >
             <span className="text-lg">گیتاب</span>
             <PiGithubLogo className="text-2xl" />
           </Link>
--- components/home/MainHome.tsx
+++ components/home/MainHome.tsx
@@ -3,14 +3,15 @@ import React from "react";
 const MainHome = () => {
   return (
     <main className="container mx-auto w-full">
-      <section className="flex flex-col items-center justify-center gap-10 py-20">
-        <h1 className="text-6xl font-bold text-neutral-700">ساخت QR Code</h1>
-        <div className="flex flex-col gap-2 text-neutral-500 text-center text-2xl">
+      <section className="flex flex-col items-center justify-center gap-10 py-10 md:py-16 2xl:py-20">
+        <h1 className="text-4xl md:text-5xl 2xl:text-6xl font-bold text-neutral-900">
+          ساخت QR Code
+        </h1>
+        <div className="flex flex-col gap-2 text-neutral-500 text-center md:text-lg lg:text-xl 2xl:text-2xl">
+          <p>کد‌های QR را سریع و رایگان ایجاد کنید.</p>
           <p>
-            کدهای QR سفارشی شده رایگان و سریع ایجاد کنید.
-          </p>
-          <p>
-            از اطلاعات تماس کلمه عبور وای فای تا هرگونه داده های متنی و لینک ها
+            اطلاعات تماس، کلمه عبور وای فای تا داده های متنی و لینک ها را به
+            صورت QR Code ایجاد کنید.
           </p>
         </div>
       </section>
